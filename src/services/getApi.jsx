63b3d98cd89ba391f97e9e840781d51efa9f6a68
pratikthapw <pratikthapw@gmail.com>
2023-10-14 01:02:40 +0545

    api keys updated

--- src/services/getApi.jsx
+++ src/services/getApi.jsx
@@ -1,13 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 import { createApi } from "unsplash-js";
 
-const UNSPLASH_API_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
+const UNSPLASH_API_KEY = "RcW0Oo8z75hmn84JuLe57XSXOWjeDFpiMqsEMxJ88PQ";
 const api = createApi({
   accessKey: UNSPLASH_API_KEY,
 });
 
 const supabaseUrl = "https://mjzbcimgxzvjsdtvhmka.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qemJjaW1neHp2anNkdHZobWthIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5NTE3MjgsImV4cCI6MjAxMjUyNzcyOH0.vvvNVxJNMd6Fc11YNz7NkwS-AAlT82bQaZwiLnYUp5o";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function getImages(submitValue, pageParam) {
